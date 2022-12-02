4e032282a65ae526c1b256a914058d89acbf67d3
Dominic <114390268+Ephraim52@users.noreply.github.com>
2022-12-02 16:54:41 +0100

    changes

--- cdkproject/vpc_stack.py
+++ cdkproject/vpc_stack.py
@@ -1,12 +1,13 @@
 from aws_cdk import (
     aws_ec2 as ec2,
-    core
+    Stack
 )
 
+from constructs import Construct
 
-class CustomVpcStack(core.Stack):
+class CustomVpcStack(Stack):
 
-    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
         # The code that defines your stack goes here
@@ -17,7 +18,7 @@ class CustomVpcStack(core.Stack):
                 availability_zones=["eu-west-2a", "eu-west-2b"],
                 nat_gateways=0,
                 subnet_configuration=[
-                    ec2.SubnetConfiguration(name="public", cidr_mask=25, subnet_type=ec2.SubnetType.PUBLIC),
+                    ec2.SubnetConfiguration(name="public", cidr_mask=25, subnet_type=ec2.SubnetType.PUBLIC)
                     # ec2.SubnetConfiguration(name="private", cidr_mask=24, subnet_type=ec2.SubnetType.PRIVATE)
                 ]
             )
@@ -28,7 +29,7 @@ class CustomVpcStack(core.Stack):
                 availability_zones=["eu-west-2a", "eu-west-2b"],
                 nat_gateways=0,
                 subnet_configuration=[
-                    ec2.SubnetConfiguration(name="public", cidr_mask=25, subnet_type=ec2.SubnetType.PUBLIC),
+                    ec2.SubnetConfiguration(name="public", cidr_mask=25, subnet_type=ec2.SubnetType.PUBLIC)
                     # ec2.SubnetConfiguration(name="private", cidr_mask=24, subnet_type=ec2.SubnetType.PRIVATE)
                 ]
             )
@@ -36,6 +37,7 @@ class CustomVpcStack(core.Stack):
         # Tag all VPC Resources
         # core.Tag.add(vpc,key="Owner",value="Dominic",include_resource_types=[])
 
+# this is the Webserver ACL which needs to be Public, accessible from the Admin server only through SSH and RDP
         network_acl_props = ec2.NetworkAcl(
             self, "Webserver-ACL",
             vpc= WebVPC,
@@ -45,39 +47,44 @@ class CustomVpcStack(core.Stack):
             subnet_selection=ec2.SubnetSelection(
                 availability_zones=["eu-west-2a", "eu-west-2b"],
                 one_per_az=False,
-                subnet_filters=[ec2.SubnetFilter],
-                subnet_group_name="subnetGroupName",
-                subnets=[ec2.Subnet],
+                subnet_type=ec2.SubnetType.PUBLIC
             )
         )
-
-        network_acl_entry = ec2.NetworkAclEntry(self, "Public Webserver",
+        # these are the entries that specify the rules on what is allowed like, inbound and outbound traffic
+        network_acl_entry = ec2.NetworkAclEntry(self, "AllowAllEgress",
+            # the CIDR range to allow or deny the subnet access
             cidr= ec2.AclCidr.any_ipv4(),
-            network_acl= ec2.NetworkAcl,
+            network_acl= network_acl_props,
+            # rule number represents the protocol rule number
             rule_number=100,
-            traffic= ec2.AclTraffic,
+            # What kind of traffic
+            traffic= ec2.AclTraffic.tcp_port(80),
 
             # the properties below are optional
+            # Inbound or outbound traffic direction
             direction=ec2.TrafficDirection.EGRESS,
-            network_acl_entry_name="Public Webserver",
+            # key-value with acl rule name
+            network_acl_entry_name="AllowAllEgress",
+            # whether the rule allows or denies the access that is specified
             rule_action=ec2.Action.ALLOW
             
         )
 
-        network_acl_entry = ec2.NetworkAclEntry(self, "Public Webserver",
-            cidr= ec2.AclCidr,
-            network_acl= ec2.NetworkAcl,
+        networkaclentry = ec2.NetworkAclEntry(self, "AllowAllIngress",
+            cidr= ec2.AclCidr.any_ipv4(),
+            network_acl= network_acl_props,
             rule_number=100,
-            traffic= ec2.AclTraffic,
+            traffic= ec2.AclTraffic.tcp_port(80),
 
             # the properties below are optional
             direction=ec2.TrafficDirection.INGRESS,
-            network_acl_entry_name="Public Webserver",
+            network_acl_entry_name="AllowAllIngress",
             rule_action=ec2.Action.ALLOW
             
         )
-
-        networkaclprops = ec2.NetworkAcl(
+# this is the part of the Admin server ACL which needs to be only accessible with a trusted IP address. 
+# from the Admin server access can be established to the Webserver only through SSH and RDP
+        AdminACL = ec2.NetworkAcl(
             self, "Admin-server-ACL",
             vpc= AdminVPC,
 
@@ -86,8 +93,6 @@ class CustomVpcStack(core.Stack):
             subnet_selection=ec2.SubnetSelection(
                 availability_zones=["eu-west-2a", "eu-west-2b"],
                 one_per_az=False,
-                subnet_filters=[ec2.SubnetFilter],
-                subnet_group_name="subnetGroupName",
-                subnets=[ec2.Subnet],
+                subnet_type=ec2.SubnetType.PUBLIC
             )
         )
